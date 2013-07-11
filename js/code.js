506bd1279d25772e57a9d6f3c6482193d0b8eb06
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2013-07-11 18:02:12 -0400

    Fixed navbar collapse and added transparency for navbar

---
+++ js/code.js
@@ -0,0 +1,8 @@
+console.log("Hi");
+
+ /**https://github.com/ccampbell/mousetrap
+       * Konami Code
+       */
+      Mousetrap.bind(['u u d d l r l r b a'], function(e) {
+           console.log("Gonzalo");
+      });
