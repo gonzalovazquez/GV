5822c7dc909939b46d15f481230e4462e4b3834f
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2013-07-14 20:50:04 -0400

    added first version of code.js

--- js/code.js
+++ js/code.js
@@ -1,22 +1,62 @@
 console.log("Hi");
 
-
-
 /**https://github.com/ccampbell/mousetrap
 * Konami Code */
 Mousetrap.bind(['u u'], function(e) {
 	console.log("Code Activated");
 
 	/*Elements to Variables */
+
 	var carousel = document.getElementById('myCarousel');
 
 	var langButtons = document.querySelectorAll(".span4");
+
+	var title = document.querySelector(".featurette-heading");
+
+	var navbar = document.querySelector(".navbar");
+
+	var footer = document.querySelector("footer");
+
+	var divider = document.querySelector(".featurette-divider");
 
 
            
     /*Remove Elements*/ 
     carousel.parentNode.removeChild(carousel);
-    langButtons.parentNode.removeChild(langButtons);
+    for(var i=0; i<langButtons.length; i++){
+    	langButtons[i].parentNode.removeChild(langButtons[i]);
+    }
+    title.parentNode.removeChild(title);
+    navbar.parentNode.removeChild(navbar);
+    footer.parentNode.removeChild(footer);
+    divider.parentNode.removeChild(divider);
 
+    /*Create Elements*/
+	var matrix = document.createElement('img');
+	matrix.src = 'http://shpintv.com/images/2013/03/the_matrix_1080p-HD-Wallpaper.jpg';
+	matrix.setAttribute('width', '100%');
+	matrix.setAttribute('height', '100%');
+	matrix.alt = 'Matrix';
+
+	var title=document.createElement("p");
+	var text = document.createTextNode("Welcome to the Matrix");
+	title.appendChild(text);
+	title.style.textAlign = "center";
+
+
+	document.body.appendChild(matrix);
+
+	document.querySelector(".row").appendChild(title);
     
+   
+   /* Not used Code */
+   /*
+   var div = document.createElement("div");
+        div.innerHTML = "Activated";
+   
+   	document.querySelector(".row").appendChild(div);
+    .appendChild(text);
+    */
+
+
       });
